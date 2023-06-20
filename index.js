981f436bf8e86c09fdc46de05ea204bb553aff14
WilliamFilh0 <williamfilho8908@gmail.com>
2023-06-20 11:24:42 -0300

    update from right to left

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width= 1024
+canvas.width = 1024
 canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
 const gravity = 0.2
 
-class Sprite{
-  constructor({position, velocity}){
+class Sprite {
+  constructor({ position, velocity }) {
     this.position = position
     this.velocity = velocity
     this.height = 150
   }
 
-  draw(){
+  draw() {
     c.fillStyle = 'red'
     c.fillRect(this.position.x, this.position.y, 50, this.height)
   }
-  update(){
+  update() {
     this.draw()
 
     this.position.x += this.velocity.x
-    this.position.y +=  this.velocity.y
+    this.position.y += this.velocity.y
 
-    if(this.position.y + this.height + this.velocity.y >= canvas.height){
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
       this.velocity.y = 0
     } else {
       this.velocity.y += gravity
@@ -34,39 +34,57 @@ class Sprite{
 }
 
 const player = new Sprite({
-  position:{
-  x: 0,
-  y:0
-},
-velocity:{
-  x: 0,
-  y:0
-}
+  position: {
+    x: 0,
+    y: 0
+  },
+  velocity: {
+    x: 0,
+    y: 0
+  }
 })
 
 
 
 const enemy = new Sprite({
-  position:{
-  x: 400,
-  y:100
-},
-velocity:{
-  x: 0,
-  y:0
-}
+  position: {
+    x: 400,
+    y: 100
+  },
+  velocity: {
+    x: 0,
+    y: 0
+  }
 })
 
 
 
 console.log(player)
 
-function animate(){
+const keys = {
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  }
+}
+let lastKey
+
+function animate() {
   window.requestAnimationFrame(animate)
   c.fillStyle = 'black'
   c.fillRect(0, 0, canvas.width, canvas.height)
   player.update()
   enemy.update()
+
+  player.velocity.x = 0
+
+  if (keys.a.pressed && lastKey === 'a') {
+    player.velocity.x = -1
+  } else if (keys.d.pressed && lastKey === 'd') {
+    player.velocity.x = 1
+  }
 }
 
 animate()
@@ -74,7 +92,24 @@ animate()
 window.addEventListener('keydown', (event) => {
   switch (event.key) {
     case 'd':
-      player.velocity.x = 1
+      keys.d.pressed = true
+      lastKey = 'd'
+      break;
+    case 'a':
+      keys.a.pressed = true
+      lastKey = 'a'
+      break;
+  }
+  console.log(event.key);
+});
+
+window.addEventListener('keyup', (event) => {
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = false
+      break;
+    case 'a':
+      keys.a.pressed = false
       break;
   }
   console.log(event.key);
