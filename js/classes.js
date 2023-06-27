e6d83947ebd394225ca2de177af7fb51e3385bf8
WilliamFilh0 <williamfilho8908@gmail.com>
2023-06-27 10:44:25 -0300

    adding shop animation

--- js/classes.js
+++ js/classes.js
@@ -1,18 +1,24 @@
 class Sprite {
-  constructor({ position, imageSrc, scale = 1 }) {
+  constructor({ position, imageSrc, scale = 1, framesMax = 1 }) {
     this.position = position
     this.width = 50
     this.height = 150
     this.image = new Image()
     this.image.src = imageSrc
     this.scale = scale
+    this.framesMax
   }
 
   draw() {
-    c.drawImage(this.image,
+    c.drawImage(
+      this.image,
+      0,
+      0,
+      this.image.width /6,
+      this.image.height,
       this.position.x,
       this.position.y,
-      this.image.width * this.scale,
+      (this.image.width / 6) * this.scale,
       this.image.height * this.scale)
   }
 
