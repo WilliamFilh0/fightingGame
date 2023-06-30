d5d3051f2af65f145a4e8d13280e981d839ab81b
WilliamFilh0 <williamfilho8908@gmail.com>
2023-06-30 11:28:37 -0300

    adding kenji player and his animation

--- index.js
+++ index.js
@@ -95,6 +95,35 @@ const enemy = new Fighter({
   offset: {
     x: -50,
     y: 0
+  },
+  imageSrc: './img/kenji/idle.png',
+  framesMax:4,
+  scale:2.5,
+  offset: {
+    x: 215,
+    y: 167
+  },
+  sprites : {
+    idle: {
+      imageSrc: './img/kenji/idle.png',
+      framesMax: 4
+    },
+    run: {
+      imageSrc: './img/kenji/Run.png',
+      framesMax: 8
+    },
+    jump: {
+      imageSrc: './img/kenji/Jump.png',
+      framesMax: 2
+    },
+    fall: {
+      imageSrc: './img/kenji/Fall.png',
+      framesMax: 2
+    },
+    attack1: {
+      imageSrc: './img/kenji/Attack1.png',
+      framesMax: 4
+    }
   }
 });
 
@@ -125,7 +154,7 @@ function animate() {
   background.update()
   shop.update()
   player.update()
- // enemy.update()
+  enemy.update()
 
   player.velocity.x = 0
   enemy.velocity.x = 0
@@ -152,8 +181,19 @@ function animate() {
   // Enemy movement
   if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
     enemy.velocity.x = -5
+    enemy.switchSprite('run')
   } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
     enemy.velocity.x = 5
+    enemy.switchSprite('run')
+  } else {
+    enemy.switchSprite('idle')
+  }
+
+  //jumping enemy
+  if(enemy.velocity.y < 0) {
+    enemy.switchSprite('jump')
+  } else if(enemy.velocity.y > 0){
+    enemy.switchSprite('fall')
   }
 
   // detect for colision
