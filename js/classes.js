97d2d04002956177c68b31ed35a4f8ff72c22e43
WilliamFilh0 <williamfilho8908@gmail.com>
2023-06-30 11:09:18 -0300

    Adding attack1 animation

--- js/classes.js
+++ js/classes.js
@@ -138,15 +138,18 @@ class Fighter extends Sprite {
   }
 
   attack() {
-    this.switchSprite('attack1');
+    this.switchSprite('attack1')
     this.isAttacking = true;
     setTimeout(() => {
       this.isAttacking = false;
     }, 100);
   }
-  
+
 
   switchSprite(sprite) {
+    if (
+      this.image === this.sprites.attack1.image && this.frameCurrent < this.sprites.attack1.framesMax -1) return
+      
     switch (sprite) {
       case 'idle':
         if (this.image !== this.sprites.idle.image) {
